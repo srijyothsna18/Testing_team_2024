f9292a24894646d5cef3c78b2824d51708cc2fb4
Veenanjali <91068676+Veenanjali@users.noreply.github.com>
2024-05-06 17:37:13 +0530

    Update log path for STM32_elements.py

--- Base/Elements/STM32_elements.py
+++ Base/Elements/STM32_elements.py
@@ -1,3 +1,3 @@
 class STM:
     PATH = r"Base/utils/Screenshots/STM_SS"
-    LOG_PATH2 = r"C:\Users\vlab\PycharmProjects\final_Framework\Base\utils\Logs\STM_logs\stm32.log"
+    LOG_PATH2 = r"Base\utils\Logs\STM_logs\stm32.log"
